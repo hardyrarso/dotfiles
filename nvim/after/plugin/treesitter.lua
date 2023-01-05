6685cc0005635f51f252ec5dfdf2e13ae685f114
Achmad Hardiansyah <achmadhs08@gmail.com>
2023-01-06 00:56:59 +0700

    remove treesitter binding -- I prefer the default vim binding

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -47,7 +47,6 @@ require'nvim-treesitter.configs'.setup {
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
         [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
       },
       goto_next_end = {
         [']M'] = '@function.outer',
@@ -55,7 +54,6 @@ require'nvim-treesitter.configs'.setup {
       },
       goto_previous_start = {
         ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
       },
       goto_previous_end = {
         ['[M'] = '@function.outer',
