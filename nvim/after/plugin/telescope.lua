52d7b26f30a3e8c799145269439049a1e5a4d6be
Achmad Hardiansyah <achmadhs08@gmail.com>
2023-01-06 10:48:15 +0700

    update telescope.nvim configuration
    - add dropdown theme to all searches
    - add mappings
    - fix mapping bug from bad syntax causing some mappings to not be
      usable

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,12 +1,18 @@
-pcall(require('telescope').load_extension, 'fzf')
-
+local actions = require("telescope.actions")
 local builtin = require('telescope.builtin')
+
 -- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
 vim.keymap.set('n', '<leader>sw', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
     end,
@@ -14,10 +20,48 @@ vim.keymap.set('n', '<leader>sw', function()
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer]' })
 
+
+require("telescope").setup({
+    defaults = {
+        file_ignore_patterns = { "go.sum" },
+        mappings = {
+            i = {
+                ["<leader>;"] = actions.close,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-n>"] = actions.cycle_history_next,
+                ["<C-p>"] = actions.cycle_history_prev,
+            },
+            n = {
+                ["<leader>;"] = actions.close,
+            },
+        },
+    },
+    pickers = {
+        find_files = {
+            theme = "dropdown",
+        },
+        old_files = {
+            theme = "dropdown",
+        },
+        buffers = {
+            theme = "dropdown",
+        },
+        grep_string = {
+            theme = "dropdown",
+        },
+        help_tags = {
+            theme = "dropdown",
+        },
+        live_grep = {
+            theme = "dropdown",
+        },
+    },
+    extensions = {
+        fzf = {},
+    }
+})
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
